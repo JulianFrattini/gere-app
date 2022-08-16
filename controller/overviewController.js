b756ce89762e3fa1e2061f3e6138c14882e297f6
Julian Frattini <julian.frattini@alice-dsl.net>
2022-08-16 14:05:39 +0200

    Added members section

--- controller/overviewController.js
+++ controller/overviewController.js
@@ -5,14 +5,14 @@ const path = require('path');
 module.exports = {
     landingPageGet: (req, res, next) => {
         try{
-            //const membersFile = fs.readFileSync(path.resolve(__dirname, '../public/docs/members.json'));
-            //const members = JSON.parse(membersFile);
+            const membersFile = fs.readFileSync(path.resolve(__dirname, '../public/docs/members.json'));
+            const members = JSON.parse(membersFile);
     
             const papersFile = fs.readFileSync(path.resolve(__dirname, '../public/docs/publications.json'));
             const publications = JSON.parse(papersFile);
     
             res.render('index', {
-                //members: members,
+                members: members,
                 publications: publications
             });
         } catch(error) {
