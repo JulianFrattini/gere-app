bcb3239a748aaf0b816fa0c2b8d8eca28bbba8c4
Julian Frattini <julian.frattini@alice-dsl.net>
2022-08-17 17:40:29 +0200

    Updated content retrieval

--- routes/extractions.js
+++ routes/extractions.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const extractionController = require('../controller/extractionController')
 
-router.get('/clone', extractionController.clone);
-router.get('/update', extractionController.update);
+//router.get('/clone', extractionController.clone);
+//router.get('/update', extractionController.update);
 
 module.exports = router
